a0c100a7cb73932950e3ca463678e1e5a46e767b
Manuel Delgado <manuel.delgado@ucr.ac.cr>
2013-07-09 14:37:28 -0600

    Added Primary key (ip_address and user_id)

--- sql/PostgreSQL.sql
+++ sql/PostgreSQL.sql
@@ -1,11 +1,15 @@
 -- PostgreSQL table for user_activity
 
 CREATE TABLE user_activity (
-	ip varchar(40) NOT NULL PRIMARY KEY,
-	first timestamp with time zone NOT NULL,
-	last timestamp with time zone NOT NULL,
-	counter integer NOT NULL
+  ip_address varchar(40) NOT NULL,
+  user_id integer NOT NULL
+        REFERENCES users (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  first timestamp with time zone NOT NULL,
+  last timestamp with time zone NOT NULL,
+  counter integer NOT NULL
+  PRIMARY KEY (ip_address, user_id)
 );
 
 CREATE INDEX user_activity_last_idx ON user_activity(last);
 CREATE INDEX user_activity_counter_idx ON user_activity(counter);
+CREATE INDEX user_activity_key_idx ON user_activity(ip_address, user_id);
